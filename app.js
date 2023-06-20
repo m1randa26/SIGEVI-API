f03e480b4763260773c48035dbdd83842c19d390
Isaac <20223TN020@utez.edu.mx>
2023-06-20 00:12:36 -0600

    API base

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+const port = 3000;
+const app = express();
+app.use(cors());
+
+db.connect(err => {
+    if (err) {
+        console.log('Error', err);
+        return;
+    }
+
+    console.log('EXITO');
+});
+
+const userRouter = require('./routes/usuarios');
+app.use('/usuarios', userRouter);
+
+app.listen(port, () => {
+    console.log(`Servidor en el puerto: ${port}`);
+});
---
+++ routes/usuarios.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const connection = require('../db');
+
+router.get('/', (req, res) => {
+    connection.query('SELECT * FROM usuario', (err, results) => {
+        if (err) {
+            console.log('Error en la consulta', err);
+            res.status(500).json({error: 'Error interno del servidor'});
+            return
+        }
+        res.json(results);
+    });
+});
+
+router.post('/', (req, res) => {
+    const { username, password } = req.body;
+
+    const query = `SELECT * FROM usuario WHERE nombre_usuario = ${username}
+    AND contraseña = ${password}`;
+
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.log('Error al realizar la consulta: ', err);
+            res.sendStatus(500);
+        } else {
+            if (results.length > 0) {
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(401);
+            }
+        }
+    });
+});
+
+module.exports =router;
